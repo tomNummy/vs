88df3dfc12cd3908e1b9cf50423389985bd093a4
tomNummy <tom@unsupervised.com>
2020-06-24 13:05:35 -0600

    cli wip

---
+++ cli.py
@@ -0,0 +1,44 @@
+import re
+
+import click
+
+from graph import build_graph
+
+
+def get_valid_filename(s):
+    """
+    Return the given string converted to a string that can be used for a clean
+    filename. Remove leading and trailing spaces; convert other spaces to
+    underscores; and remove anything that is not an alphanumeric, dash,
+    underscore, or dot.
+    >>> get_valid_filename("john's portrait in 2004.jpg")
+    'johns_portrait_in_2004.jpg'
+    """
+    s = str(s).strip().replace(" ", "_")
+    return re.sub(r"(?u)[^-\w.]", "", s)
+
+
+@click.command(name="vs-graph")
+@click.argument("term")
+@click.option(
+    "--radius",
+    "-r",
+    type=click.IntRange(min=1, max=10, clamp=False),
+    required=False,
+    default=3,
+)
+@click.option("--fpath", "-f", type=click.Path(), required=False, default=None)
+@click.pass_context
+def vs_graph(term, radius, fpath):
+    """Build a neighborhood graph of the search term
+
+    \b
+    Args:
+        term: Word to search for
+        radius: Maximum distance away from the central node to keep in the graph
+        fpath: Where to save the html output.
+    """
+
+    if not fpath:
+        fpath = get_valid_filename(term) + ".html"
+    g = build_graph()
--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 setup(
-    name='vs-graph',
+    name="vs-graph",
     author="tomNummy",
     author_email="tom.nummy@gmail.com",
     description="A small tool for visualizing concept graphs us Google search",
     url="https://github.com/tomNummy/vs-graph",
-    version='0.1.0',
-    packages=['networkx', 'click'],
-    license='MIT License'
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["networkx", "click", "pydantic", "bokeh"],
+    license="MIT License",
+    entry_points={"console_scripts": ["vs-graph=cli:vs-graph"]},
 )
